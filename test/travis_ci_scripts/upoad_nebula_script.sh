f08b4a6347751f9e483c7a945de23e1b60588b2b
jakobpederson <jakob.pederson@gmail.com>
2019-02-12 16:39:41 -0600

    test run

---
+++ test/travis_ci_scripts/upoad_nebula_script.sh
@@ -0,0 +1,19 @@
+#go to home and setup git
+cd $HOME
+git config --global user.email $USER_EMAIL
+git config --global user.name $USER_NAME
+
+#clone the repository in the buildApk folder
+git clone --quiet --branch=master  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+
+cd master
+pyinstaller -F nebulactl.py
+
+#add, commit and push files
+git add -f .
+git remote rm origin
+git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed"
+git push --quiet --set-upstream origin-pages gh-pages
+echo -e "Done"
