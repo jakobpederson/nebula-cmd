94da60538e94130fe18886179dc87eba812ec4b5
jakobpederson <jakob.pederson@gmail.com>
2019-02-12 17:19:12 -0600

    Test: attempt to stop constant uploading

--- test/travis_ci_scripts/upload_nebula_script.sh
+++ test/travis_ci_scripts/upload_nebula_script.sh
@@ -12,7 +12,7 @@ pyinstaller -F nebulactl.py
 #add, commit and push files
 git add -f .
 git remote rm origin
-git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git
+git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git > /dev/null 2>&1
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed"
 git push --quiet --set-upstream origin test-branch-1
