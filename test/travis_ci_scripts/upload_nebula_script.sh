3ffbe123e45c3f42b620f8510d178481612b8de6
jakobpederson <jakob.pederson@gmail.com>
2019-02-12 16:55:34 -0600

    Test: update github

--- test/travis_ci_scripts/upload_nebula_script.sh
+++ test/travis_ci_scripts/upload_nebula_script.sh
@@ -4,9 +4,9 @@ git config --global user.email $USER_EMAIL
 git config --global user.name $USER_NAME
 
 #clone the repository in the buildApk folder
-git clone --quiet --branch=master  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+git clone --quiet --branch=test-branch-1  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
 
-cd master
+cd test-branch-1
 pyinstaller -F nebulactl.py
 
 #add, commit and push files
@@ -15,5 +15,5 @@ git remote rm origin
 git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed"
-git push --quiet --set-upstream origin-pages gh-pages
+git push --quiet --set-upstream origin test-branch-1
 echo -e "Done"
