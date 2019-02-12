b2a7d801e120f06250325eaae1675765a98b0840
jakobpederson <jakob.pederson@gmail.com>
2019-02-12 17:10:18 -0600

    Test: step back

--- test/travis_ci_scripts/upload_nebula_script.sh
+++ test/travis_ci_scripts/upload_nebula_script.sh
@@ -3,12 +3,18 @@
 #git config --global user.email $USER_EMAIL
 #git config --global user.name $USER_NAME
 
+<<<<<<< Updated upstream
 ##clone the repository in the buildApk folder
 #git clone --quiet --branch=master  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+=======
+#clone the repository in the buildApk folder
+git clone --quiet --branch=test-branch-1  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+>>>>>>> Stashed changes
 
 #cd master
 #pyinstaller -F nebulactl.py
 
+<<<<<<< Updated upstream
 ##add, commit and push files
 #git add -f .
 #git remote rm origin
@@ -38,3 +44,13 @@ upload_files() {
 setup_git
 commit_website_files
 upload_files
+=======
+#add, commit and push files
+git add -f .
+git remote rm origin
+git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed"
+git push --quiet --set-upstream origin test-branch-1
+echo -e "Done"
+>>>>>>> Stashed changes
