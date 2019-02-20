dc392cd85a48e3b5c6ee6d7317acfe6464dd35f7
jakobpederson <jakob.pederson@gmail.com>
2019-02-20 16:21:02 -0600

    wip

--- test/travis_ci_scripts/build_and_deploy.sh
+++ test/travis_ci_scripts/build_and_deploy.sh
@@ -9,10 +9,7 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ]; then
     echo "user not approved"
     exit 0
 fi
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "$TRAVIS_PULL_REQUEST"
-    echo "$TRAVIS_BRANCH"
-    echo "$SOURCE_BRANCH"
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
 fi
