e7668d2fee9ff4cbf3292c815386f555d5d81bee
jakobpederson <jakob.pederson@gmail.com>
2019-02-19 18:15:12 -0600

    wip: small fixes

--- test/travis_ci_scripts/build_and_deploy.sh
+++ test/travis_ci_scripts/build_and_deploy.sh
@@ -11,7 +11,6 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ]; then
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
-fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
