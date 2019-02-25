bd46a76e33ce78ecfc9d5d0dcc12cc62e275c612
jakobpederson <jakob.pederson@gmail.com>
2019-02-25 17:35:49 -0600

    wip

---
+++ test/travis_ci_scripts/upload_nebula_script.sh
@@ -0,0 +1,25 @@
+#go to home and setup git
+cd $HOME
+git config --global user.email $USER_EMAIL
+git config --global user.name $USER_NAME
+
+#clone the repository in the buildApk folder
+git clone --quiet --branch=$TRAVIS_BRANCH  https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd  master > /dev/null
+
+
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+
+if [[ "$MESSAGE" == *"Travis build"* ]] || [[ "$MESSAGE" == *"SKIP TRAVIS"* ]]; then
+    echo "already pushed"
+    exit 0
+else
+    #add, commit and push files
+    cd master
+    pyinstaller -F nebulactl.py
+    git remote rm origin
+    git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git > /dev/null 2>&1
+    git add dist
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed SKIP TRAVIS"
+    git push --quiet --set-upstream origin $TRAVIS_BRANCH
+    echo -e "Done"
+fi
