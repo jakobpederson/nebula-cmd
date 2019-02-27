e617916f7b35061926790dd7660e780380d22592
jakobpederson <jakob.pederson@gmail.com>
2019-02-26 19:12:09 -0600

    wip

--- test/travis_ci_scripts/upload_nebula_script.sh
+++ test/travis_ci_scripts/upload_nebula_script.sh
@@ -15,10 +15,10 @@ if [[ "$MESSAGE" == *"Travis build"* ]] || [[ "$MESSAGE" == *"SKIP TRAVIS"* ]]; 
     echo "already pushed"
 else
     #add, commit and push files
-    git add -f .
+    git add -f dist/
     git remote rm origin
     git remote add origin https://$USER_NAME:$GITHUB_API_KEY@github.com/$USER_NAME/nebula-cmd.git > /dev/null 2>&1
-    git add -f .
+    git add -f dist/
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed - nebulactl.py run and pushed SKIP TRAVIS"
     git push --quiet --set-upstream origin $TRAVIS_BRANCH
     echo -e "Done"
